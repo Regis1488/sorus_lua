aa2b9e197e2c6ff271d2140662a6902f79c794e8
Regis1488 <82665748+Regis1488@users.noreply.github.com>
2022-03-28 17:37:44 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,5 +1,6 @@
 --[[
     Made by ₷orus#8059
+    Credits : pred14 - For ping and thanks to your code to better understand the API
     Get Good Get Lmaobox.
 ]]
 
@@ -8,6 +9,7 @@
 
 local consolas = draw.CreateFont("Consolas", 17, 500)
 local name
+local prefix = "$"
 
 if(entities.GetLocalPlayer() == nil) then
     return
@@ -199,27 +201,15 @@ local function DetectMessage(msg)
 
         local chatType = msg:ReadString(256)
         local playerName = msg:ReadString(256)
-        local message = msg:ReadString(256)
-        local date = os.date()
-        
-        if string.find(message, "!date") then
-
-            client.ChatSay(date)
-            client.Command("tf_party_chat \""..date.. "\"", true)
-        end -- Command "date", send the current date
-
-
-        if string.find(message, "!number") then
-            local random = math.random(1,1000)
-            client.ChatSay(random)
-         
-                   end -- Command "!number" send a random numer beetwen 1 and 1000
+        local message = msg:ReadString(256)         
+                 
 
         if string.find(message, "is a cheater") or string.find(message, "cheater") or string.find(message, "kick him") or string.find(message, "cheater") or string.find(message, "kick " .. info) then
       detect = 1
         end -- Check every message send, and find cheaters and accuse words, and set detect to 1
     end
 
+  
 
 end
 
@@ -227,10 +217,108 @@ local function AutoKick(ev)
     if(detect == 1) then
         detect = 0 
     local victim_remains = ev:GetInt("userid")
-    client.Command("callvote kick " .. victim_remains)
+  --  client.Command("callvote kick " .. victim_remains)
     print("Nigger detected !")
     
   end
+
+
+if(ev:GetName() == "party_chat") then
+    local eventMessage = ev:GetString("text")
+   if(eventMessage == prefix .. "date" ) then
+    local issou = os.date()
+    client.Command("tf_party_chat \""..issou.. "\"", true)
+   end
+
+   if(eventMessage == prefix .. "number") then
+    local random = math.random(1,1000)
+    client.Command("tf_party_chat \""..random.. "\"", true)
+   end
+
+   
+   if(eventMessage == prefix .. "help") then
+    local help = prefix .. "help : Send this message"
+    local startqueue = prefix .. "queue : Start queue"
+    local date = prefix .. "date : Send the date"
+    local stopqueue = prefix .. "stop : Stop queue"
+    local number = prefix .. "humber : Send a random number beetween 1 and 1000"
+    local members = prefix .."members : Give current members in the party"
+    local clearChats = prefix .. "clear : Clear the party chat"
+    local PendingMb = prefix .. "pm : Send Pendings members"
+    local ping = prefix .. "ping : Send the ping"
+    local ball = prefix .. "8ball : Send Answer"
+    client.Command("tf_party_chat \""..help.. "\"", true)
+    client.Command("tf_party_chat \""..date.. "\"", true)
+    client.Command("tf_party_chat \""..number.. "\"", true)
+    client.Command("tf_party_chat \""..members.. "\"", true)
+    client.Command("tf_party_chat \""..startqueue.. "\"", true)
+    client.Command("tf_party_chat \""..stopqueue.. "\"", true)
+    client.Command("tf_party_chat \""..clearChats.. "\"", true)
+    client.Command("tf_party_chat \""..PendingMb.. "\"", true)
+    client.Command("tf_party_chat \""..ping.. "\"", true)
+    client.Command("tf_party_chat \""..ball.. "\"", true)
+   
+   end
+
+   if(eventMessage == prefix .. "queue") then
+    party.QueueUp(7)
+   end
+   if(eventMessage == prefix .. "stop") then
+    party.CancelQueue(7)
+end
+if(eventMessage == prefix .. "clear") then
+    local msg = "                                                                            "
+    client.Command("tf_party_chat \"".. msg .."\"", true)
+end
+if(eventMessage == prefix .. "pending") then
+    local errorp = "No pending Members"
+local p = table.concat(party.GetPendingMembers())
+if(p == "") then
+    client.Command("tf_party_chat \""..errorp.. "\"", true)
+    return
+end
+client.Command("tf_party_chat \""..p.. "\"", true)
+end
+if(eventMessage == prefix .. "ping") then
+    local me = entities.GetLocalPlayer()
+
+    local ping = entities.GetPlayerResources():GetPropDataTableInt("m_iPing")[me:GetIndex()] 
+    local message = "Current Ping : " .. ping
+    client.Command("tf_party_chat \""..message.. "\"", true)
+end
+if(eventMessage == prefix .. "8ball") then
+    local err = "Needs args"
+   client.Command("tf_party_chat \"".. err .."\"", true)
+    return
+elseif string.find(eventMessage,prefix .. "8ball") then
+        if(string.find(eventMessage, "8ball : Send Answer")) then
+            return
+        end
+
+         local random = math.random(1,6)
+        local answers = {"Yes!", "Probably", "Probably not", "No!", "I'm sure", "I'm not sure"}
+      local answersFinal = "Result : " .. answers[random]
+      client.Command("tf_party_chat \""..answersFinal.. "\"", true)
+end
+
+
+   if(eventMessage == prefix .."members") then
+
+  if party.GetLeader() == nil then
+    return client.ChatPrintf("\x0700eeffYou are not in party. ")
+    end
+    local p = table.concat(party.GetMembers())
+    
+   local x = steam.GetPlayerName(p)
+   local send = "Current Members : " .. x .. ", "
+   print(send)
+   client.Command("tf_party_chat \""..send.. "\"", true)
+   end
+   
+end
+
+
+
 end
 
 
