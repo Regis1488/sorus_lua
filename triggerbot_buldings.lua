ae4bdd372adaffb6f261d06f4cf741590f01eaca
Regis1488 <82665748+Regis1488@users.noreply.github.com>
2023-02-24 14:24:19 +0100

    Update triggerbot_buldings.lua

--- triggerbot_buldings.lua
+++ triggerbot_buldings.lua
@@ -1,13 +1,16 @@
 local function triggerBot(cmd)
     local localPlayer = entities.GetLocalPlayer()
-    local test = Vector3(localPlayer:GetPropFloat("localdata", "m_vecViewOffset[0]"), localPlayer:GetPropFloat("localdata", "m_vecViewOffset[1]"), localPlayer:GetPropFloat("localdata", "m_vecViewOffset[2]"))
-    local source = localPlayer:GetAbsOrigin() + test;
+    local vec = Vector3(localPlayer:GetPropFloat("localdata", "m_vecViewOffset[0]"), localPlayer:GetPropFloat("localdata", "m_vecViewOffset[1]"), localPlayer:GetPropFloat("localdata", "m_vecViewOffset[2]"))
+
+    local source = localPlayer:GetAbsOrigin() + vec;
     local destination = source + engine.GetViewAngles():Forward() * 5000;
+
     local triggerKey = gui.GetValue("Trigger Shoot Key")
+
     if(input.IsButtonDown(triggerKey)) then
         local trace = engine.TraceLine(source,destination,MASK_SHOT)
         
-        if(trace.entity:GetClass() == "CObjectSentrygun" or trace.entity:GetClass() == "CObjectDispenser" or trace.entity:GetClass() == "CObjectTeleporter") then
+        if(trace.entity == "CObjectSentrygun" or trace.entity== "CObjectDispenser" or trace.entity == "CObjectTeleporter") then
             cmd.buttons = cmd.buttons | IN_ATTACK
         end
     end
